2c7373b272576518911182189e964186a6ab815c
VVNTHEFLASH <vishnurockhunters@gmail.com>
2022-07-11 04:30:10 +0530

    about and help

--- client/src/components/dashboard/dash-components/About.js
+++ client/src/components/dashboard/dash-components/About.js
@@ -9,7 +9,18 @@ export default class About extends Component {
             E-Voting System Application is Live.
           </div>
           <div>
-            Updates will be avaliable soon!
+            <h4>About</h4>
+            <div>
+              <ul>
+                Project Details
+                <li>
+                Author: Vishnu Vardhan
+                </li>
+                <li>Frontend: React JS</li>
+                <li>Backend: Node JS and Express</li>
+                <li>Database: Mongo db Atlas</li>
+              </ul>
+              </div> 
           </div>
         </div>
         </>
--- client/src/components/dashboard/dash-components/Help.js
+++ client/src/components/dashboard/dash-components/Help.js
@@ -3,7 +3,24 @@ import React, { Component } from 'react'
 export default class Help extends Component {
   render() {
     return (
-      <div>Help
+      <div>
+        <div>
+          <h3>Help!</h3>
+          <div>
+            <h4>Instruction</h4>
+            <div>
+              <blockquote>
+              <ul>
+                <li>User Must Copy Unique ID from Profile Page in order to Cast Vote.</li>
+                <li>Paste the copied Id in Vote link, input bar.</li>
+                <li>Select your Favorite Party and press Vote button to Cast Vote.</li>
+                <li>After Voting You will redirected to live result page, where you can see who is leading party.</li>
+                <li>After Voting Make Sure to Logout.</li>
+              </ul>
+              </blockquote>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
